bf4436856680aad5682e9c91ac44c452dde93d85
djamalsoc-2000 <djamalsoc@gmail.com>
2026-07-23 18:04:58 -0400

    Integrate Extent Reports - HTML dashboard with pass/fail summary and timeline

--- src/test/java/listeners/TestListener.java
+++ src/test/java/listeners/TestListener.java
@@ -9,24 +9,41 @@ import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
 import driver.DriverFactory;
+import reports.ExtentManager;
 
 public class TestListener implements ITestListener {
 
+    private static ExtentReports extent = ExtentManager.getInstance();
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
     @Override
     public void onTestStart(ITestResult result) {
+        ExtentTest extentTest = extent.createTest(result.getMethod().getMethodName());
+        test.set(extentTest);
         System.out.println("STARTING TEST: " + result.getMethod().getMethodName());
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
+        test.get().log(Status.PASS, "Test passed");
         System.out.println("PASSED: " + result.getMethod().getMethodName());
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
+        test.get().log(Status.FAIL, result.getThrowable());
         System.out.println("FAILED: " + result.getMethod().getMethodName());
         takeScreenshot(result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onFinish(org.testng.ITestContext context) {
+        extent.flush();
     }
 
     private void takeScreenshot(String testName) {
